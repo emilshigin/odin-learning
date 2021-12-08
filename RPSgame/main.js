e814e8c3c7bca998bcb5c0f99f30a1ce006ea35a
emilshigin <emilshigin@protonmail.com>
2021-12-07 20:34:03 -0800

    idk stuff

--- RPSgame/main.js
+++ RPSgame/main.js
@@ -3,53 +3,54 @@ let roundOrder = ["First", "Second", "Third", "Fourth", "Final"];
 let playerChoice,computerChoice,winner,gameResult, round = 0, playerPoints = 0, computerPoints = 0; 
 
 function game(Element){
-    if(round > 4){round = 0,playerPoints = 0, computerPoints = 0}
+    //player card and change card element: title and image
     playerChoice = Element.id;
-    // change player card  element
-
     document.getElementById("playerSelected").innerHTML = `${playerChoice}`;
     document.getElementById("playerSelected").style.backgroundImage = `url(img/${playerChoice}.png)`;
-    // computer choice
+   
+    // computer choice and change card element: title and image
     computerChoice = options[Math.floor(Math.random()*3)];
     document.getElementById("computerSelected").innerHTML = `${computerChoice}`;
     document.getElementById("computerSelected").style.backgroundImage = `url(img/${computerChoice}.png)`;
     
     winner = rules(playerChoice,computerChoice);
-    if(winner === "Tie"){
+
+    // Game result
+    if(winner === "tie"){
         document.getElementById("winner").innerHTML = `We will do ${roundOrder[round]} round again since it was a ${winner}`;
-    }else{
+    }
+    else{
         document.getElementById("winner").innerHTML = `${roundOrder[round++]} round winner is the ${winner}`;
     }
-
+    
     // update points
     document.getElementById("playerScore").innerHTML = `Player Score: ${playerPoints}`;
     document.getElementById("computerScore").innerHTML = `Computer Score: ${computerPoints}`;
     document.getElementById('playByPlay').innerHTML = `Player Choice: ${playerChoice} vs Computer Choice: ${computerChoice}`;
+    
+    // winner condition
     if(playerPoints === 3 || computerPoints === 3 || round === 5){
         alert('The Winner is ' + (playerPoints > computerPoints ? 'Player' : 'Computer'));
+        round = 0,playerPoints = 0, computerPoints = 0;
+        document.getElementById("winner").innerHTML = `Lets play again`;
     }
-
-
 }
 
 function rules(playerChoice,computerChoice){
     if(playerChoice === computerChoice){
-        return "Tie";
-    }else if(playerChoice === "Rock" && computerChoice === "Scissors"){
+        return "tie";
+    }else if(playerChoice === "rock" && computerChoice === "scissor"){
         playerPoints++;
-        return "Player";
-    }else if(playerChoice === "Paper" && computerChoice === "Rock"){
+        return "player";
+    }else if(playerChoice === "paper" && computerChoice === "rock"){
         playerPoints++;
-        return "Player";
-    }else if(playerChoice === "Scissors" && computerChoice === "Paper"){
+        return "player";
+    }else if(playerChoice === "scissor" && computerChoice === "paper"){
         playerPoints++;
-        return "Player";
+        return "player";
     }else{
-        computerPoints++;
-        return "Computer";
+        computerPoints++;round
+        return "computer";
     }
 }
 
-function points(){
-
-}
